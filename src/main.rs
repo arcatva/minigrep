444dadfaa97fc256d39276bd6e2d97e2426853c2
Zhefu Zhang <41334845+arcatva@users.noreply.github.com>
2024-04-08 01:31:59 +1200

    .

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+	let args: Vec<String> = env::args().collect();
+	let (query, file_path) = parse_config(&args);
+
+
+	let contents = String::from_utf8(fs::read(file_path).expect("Should have been able to read the file")).expect("Not UTF-8 string");
+
+
+	println!("Searching for {}", query);
+	println!("In file {}", file_path);
+	println!("With text:\n{contents}");
 }
+
+struct Config<'a> {
+	query: &'a String,
+	file_path: &'a String,
+}
+
+fn parse_config(args: &Vec<String>) -> Config {
+	let query = &args[1];
+	let file_path = &args[2];
+	Config {
+		query,
+		file_path,
+	}
+}
+
