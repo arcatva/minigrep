1e7047d80f834904adfe798057d1627e078ee110
Zhefu Zhang <41334845+arcatva@users.noreply.github.com>
2024-04-08 01:38:27 +1200

    .

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,25 @@ use std::{env, fs};
 
 fn main() {
 	let args: Vec<String> = env::args().collect();
-	let (query, file_path) = parse_config(&args);
+	let config = parse_config(args);
 
 
-	let contents = String::from_utf8(fs::read(file_path).expect("Should have been able to read the file")).expect("Not UTF-8 string");
+	let contents = String::from_utf8(fs::read(&config.file_path).expect("Should have been able to read the file")).expect("Not UTF-8 string");
 
 
-	println!("Searching for {}", query);
-	println!("In file {}", file_path);
+	println!("Searching for {}", config.query);
+	println!("In file {}", config.file_path);
 	println!("With text:\n{contents}");
 }
 
-struct Config<'a> {
-	query: &'a String,
-	file_path: &'a String,
+struct Config {
+	query: String,
+	file_path: String,
 }
 
-fn parse_config(args: &Vec<String>) -> Config {
-	let query = &args[1];
-	let file_path = &args[2];
+fn parse_config(args: Vec<String>) -> Config {
+	let query = args[1].clone();
+	let file_path = args[2].clone();
 	Config {
 		query,
 		file_path,
