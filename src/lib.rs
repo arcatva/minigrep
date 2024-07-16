59c8c5fc14ffdb23212eef532eb641d63c52059a
Zhefu Zhang <a723356@gmail.com>
2024-07-16 23:54:33 +1200

    feat: complete search function

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,42 @@
+use std::error::Error;
+use std::fs;
+
 pub struct Config {
-	pub query: String,
-	pub file_path: String,
+    pub query: String,
+    pub file_path: String,
 }
 
 impl Config {
-	pub fn build(args: &[String]) -> Result<Config, &'static str> {
-		if args.len() < 3 {
-			return Err("not enough arguments");
-		}
-		Ok(Config {
-			query: args[1].clone(),
-			file_path: args[2].clone(),
-		})
-	}
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        Ok(Config {
+            query: args[1].clone(),
+            file_path: args[2].clone(),
+        })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)
+        .expect("Should have been able to read the file");
+    println!("With text:\n{contents}");
+    for i in search(&config.query, &contents) {
+        println!("Matched: {}", i);
+    }
+
+    Ok(())
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-	for line in contents.lines() {
-		if line.contains(query) {}
-	}
-	return;
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line.trim())
+        }
+    }
+    results
 }
 
 #[cfg(test)]
@@ -27,13 +44,13 @@ mod tests {
 	use super::*;
 
 	#[test]
-	fn one_result() {
-		let query = "duct";
-		let contents = "\
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
 		Rust:
 		safe, fast, productive.
 		Pick three.";
-		assert_eq!(vec!["safe, fast, productive."], search(query, contents));
-	}
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,21 @@
-use std::{env, fs, process};
-use std::error::Error;
+use std::{env, process};
 
-use minigrep::Config;
+use minigrep::{Config, run};
 
 fn main() {
-	let args: Vec<String> = env::args().collect();
-	let config = Config::build(&args).unwrap_or_else(
-		|err| {
-			println!("Problem parsing arguments: {err}");
-			process::exit(1);
-		}
-	);
-	println!("Searching for {}", config.query);
-	println!("In file {}", config.file_path);
-	run(config);
+    let args: Vec<String> = env::args().collect();
+    let config = Config::build(&args).unwrap_or_else(
+        |err| {
+            println!("Problem parsing arguments: {err}");
+            process::exit(1);
+        }
+    );
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+    run(config);
 }
 
 
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-	let contents = fs::read_to_string(config.file_path)
-		.expect("Should have been able to read the file");
-	println!("With text:\n{contents}");
-	Ok(())
-}
+
 
 
