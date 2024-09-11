c70516356eab4b31d428421a10a67d48b43fd7c5
Zhefu Zhang <a723356@gmail.com>
2024-09-12 01:17:13 +0800

    feat: Update Config struct and build function
    - Add new field `mode` to the `Config` struct representing the search mode
    - Modify the `build` function to handle the new `mode` field and optional `ignore_case` field
    - Implement the `match_mode` function to convert command-line argument to `Mode` enum

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,73 @@
-use std::error::Error;
-use std::fs;
+use std::{env, error::Error, fs};
+#[cfg(test)]
+mod tests;
+
+pub enum Mode {
+    IgnoreCase,
+    Invalid,
+    None,
+}
 
 pub struct Config {
+    pub mode: Mode,
     pub query: String,
     pub file_path: String,
+    pub ignore_case: Option<bool>,
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+    pub fn build(mut args: Vec<String>) -> Result<Config, &'static str> {
         if args.len() < 3 {
             return Err("not enough arguments");
         }
-        Ok(Config {
-            query: args[1].clone(),
-            file_path: args[2].clone(),
-        })
+
+        match args.len() {
+            3 => Ok(Config {
+                mode: Mode::None,
+                query: args.remove(1),
+                file_path: args.remove(1),
+                ignore_case: match env::var("IGNORE_CASE") {
+                    Ok(val) => Some(val.eq("1")),
+                    Err(_) => None,
+                },
+            }),
+            4 => Ok(Config {
+                mode: match_mode(args.remove(1)),
+                query: args.remove(1),
+                file_path: args.remove(1),
+                ignore_case: match env::var("IGNORE_CASE") {
+                    Ok(val) => Some(val.eq("1")),
+                    Err(_) => None,
+                },
+            }),
+
+            0..=2 => {
+                return Err("not enough arguments");
+            }
+            _ => {
+                return Err("too many arguments");
+            }
+        }
     }
 }
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)
-        .expect("Should have been able to read the file");
+    let contents =
+        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+
+    println!("\n===================");
     println!("With text:\n{contents}");
-    for i in search(&config.query, &contents) {
+    println!("===================\n");
+    let results = match config.ignore_case {
+        Some(true) => search_case_insensitive(&config.query, &contents),
+        Some(false) => search(&config.query, &contents),
+        None => match config.mode {
+            Mode::IgnoreCase => search_case_insensitive(&config.query, &contents),
+            _ => search(&config.query, &contents),
+        },
+    };
+
+    for i in results {
         println!("Matched: {}", i);
     }
 
@@ -39,18 +84,20 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     results
 }
 
-#[cfg(test)]
-mod tests {
-	use super::*;
-
-	#[test]
-    fn one_result() {
-        let query = "duct";
-        let contents = "\
-		Rust:
-		safe, fast, productive.
-		Pick three.";
-        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    let query = query.to_ascii_lowercase();
+    for line in contents.lines() {
+        if line.to_ascii_lowercase().contains(&query) {
+            results.push(line.trim())
+        }
     }
+    results
 }
 
+fn match_mode(mode: String) -> Mode {
+    match mode.as_str() {
+        "-i" => Mode::IgnoreCase,
+        _ => Mode::Invalid,
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,17 @@
+use minigrep::{run, Config};
+use std::error::Error;
 use std::{env, process};
 
-use minigrep::{Config, run};
-
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
-    let config = Config::build(&args).unwrap_or_else(
-        |err| {
-            println!("Problem parsing arguments: {err}");
-            process::exit(1);
-        }
-    );
+
+    let config = Config::build(args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
-    run(config);
+
+    run(config)
 }
-
-
-
-
-
